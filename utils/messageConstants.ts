d4958f03e0133b63543e175ee5864184048216ef
Nitesh Sethi <nonusethi1272@gmail.com>
2022-12-14 16:37:05 +0530

    Changed port of mail message since frontend is running on different port.

--- utils/messageConstants.ts
+++ utils/messageConstants.ts
@@ -1,11 +1,9 @@
-import { portNumber } from "./config";
-
 export const signupMessage = (token: string) => `
   <p>Hi, please verify your account by click on the link given below</p>
-  <a href="http://localhost:${portNumber}/verify-account/${token}" target="_blank">Click here to verify</a>
+  <a href="http://localhost:5173/verify-account/${token}" target="_blank">Click here to verify</a>
 `;
 
 export const resetPasswordMessage = (token: string) => `
   <p>You have requested for password reset. Given below the link for reset the password</p>
-  <a href="http://localhost:${portNumber}/reset-password/${token}" target="_blank">Click here to reset</a>
+  <a href="http://localhost:5173/reset-password/${token}" target="_blank">Click here to reset</a>
 `;
