1403a14beb3d4479aaef284cc073aa40a25b233a
Nitesh Sethi <nonusethi1272@gmail.com>
2023-04-02 18:10:06 +0530

    Changed messageConstants url to webiste URL

--- utils/config.ts
+++ utils/config.ts
@@ -7,3 +7,4 @@ export const userEmailNodemailer = process.env.USER_EMAIL_NODEMAILER;
 export const passwordNodemailer = process.env.APP_PASSWORD_NODEMAILER;
 export const razorpayKeyId = process.env.RAZORPAY_KEY_ID;
 export const razorpaySecret = process.env.RAZORPAY_SECRET;
+export const websiteUrl = process.env.WEBSITE_URL;
--- utils/messageConstants.ts
+++ utils/messageConstants.ts
@@ -1,9 +1,11 @@
+import { websiteUrl } from "./config";
+
 export const signupMessage = (token: string) => `
   <p>Hi, please verify your account by click on the link given below</p>
-  <a href="http://localhost:5173/verify-account/${token}" target="_blank">Click here to verify</a>
+  <a href="${websiteUrl}/verify-account/${token}" target="_blank">Click here to verify</a>
 `;
 
 export const resetPasswordMessage = (token: string) => `
   <p>You have requested for password reset. Given below the link for reset the password</p>
-  <a href="http://localhost:5173/reset-password/${token}" target="_blank">Click here to reset</a>
+  <a href="${websiteUrl}/reset-password/${token}" target="_blank">Click here to reset</a>
 `;
